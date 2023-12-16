7bd794642df50e65e1eca1e26cbd0d59ed1b692e
Surat SS <suratsamanta@gmail.com>
2023-12-16 22:44:21 +0530

    hmm

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -9,6 +11,10 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(max_length=100, null=True)
     is_bestselling = models.BooleanField(default = False)
+    slug = models.SlugField(default="",blank=True, null = False, db_index=True)
     
     def __str__(self):
         return f"{self.title} ({self.rating})"
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
